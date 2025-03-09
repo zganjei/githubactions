fe1e6509fd51e415195eaeb7a00ddd1379f47d52
Zeinab Ganjei <zganjei@users.noreply.github.com>
2025-03-09 12:08:04 +0000

    changing structure cont.

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ def test_home():
     response=app.test_client().get("/")
 
     assert response.status_code==200
-    assert response.data==b"Hello World!"
+    assert response.data==b"Hello World!" 
